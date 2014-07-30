aa12fcfcff4e8fcf7e04b7ed4b912481b3717c6e
Gwen Au <quyen_au@hotmail.com>
2014-07-30 19:24:57 +0100

    Songs showing on index and user id validated by changing before save to before create

--- project_1_app/app/models/comment.rb
+++ project_1_app/app/models/comment.rb
@@ -1,6 +1,15 @@
 class Comment < ActiveRecord::Base
+  before_create :save_user
+  
   attr_accessible :comment_content, :song_id, :user_id
+
+  validates :user_id, presence: :true
 
   belongs_to  :song
   belongs_to  :user
+
+  def save_user
+    self.user_id = current_user.id
+  end
+
 end
