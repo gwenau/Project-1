ed5ee8e7fff7aa31dadd33e2283afca533274431
Gwen Au <quyen_au@hotmail.com>
2014-07-31 19:19:48 +0100

    Before attempting to destroy artist associations.

--- project_1_app/app/controllers/comments_controller.rb
+++ project_1_app/app/controllers/comments_controller.rb
@@ -21,24 +21,27 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     @comment.save
       redirect_to song_path(@song)
-
-    # @comment = current_user.comments.create(params[:comment])
-    # @comment.user_id = current_user.id
-    # @comment.song_id = song.id
-    # @comment.save
-    # redirect_to song_path(@comment.song.id)
   end
 
   def edit
     @comment = Comment.find(params[:id])
+    @song = Song.find(params[:song_id])
+    # @comment = @song.comments.build
   end
 
   def update 
-    comment = Comment.find(params[:id])
-    comment.user_id = current_user.id
-    comment.save
-    comment.update_attributes(params[:comment])
-    redirect_to song_path(comment.song.id) 
+    @song = Song.find(params[:song_id])
+    @comment = @song.comments(params[:comment])
+    # @comment.save
+    # @comment.user_id = current_user.id
+    # @comment.update_attributes(params[:comment])
+      redirect_to song_path(@song)
+
+    # comment = Comment.find(params[:id])
+    # comment.user_id = current_user.id
+    # comment.save
+    # comment.update_attributes(params[:comment])
+    # redirect_to song_path(comment.song.id) 
   end
 
   def destroy
